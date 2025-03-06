8574653cf78e792f47ab9062dec05523f655e243
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-03-06 19:13:43 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="UTF-8" <?php bloginfo('charset')?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+</head>
+<body <?php body_class();?>>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, exercitationem.</p>
+
+    <!-- Header -->
+
+    <header>
+
+        <!-- First Section -->
+        <section class="top-bar">
+            <div class="logo">
+                LOGO
+            </div>
+
+            <div class="searchbox">
+                SEARCH
+            </div>
+        </section>
+
+        <!-- Second Section -->
+        <section class="menu-area">
+            <nav class="main-menu">
+                MENU
+            </nav>
+        </section>
+
+    </header>
