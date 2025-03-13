07b28fc8b95d1bb041560c22fb2f5f014f8e8730
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-03-13 19:12:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
-<?php get_header();?>
-   
-   <!-- Hero Section -->
-    <div id="content" class="site-content">
-        <div id="primary" class="content-area">
-            <main id="main" class="side-main">
-                <section class="hero">
-                    Hero
-                </section>
+<?php get_header(); ?>
 
-                <section class="services">
-                    Services
-                </section>
+<!-- Hero Section -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="side-main">
+            <section class="hero">
+                Hero
+            </section>
 
-                <section class="home-blog">
-                    Blog
-                </section>
-            </main>
-        </div>
+            <section class="services">
+                Services
+            </section>
+
+            <section class="home-blog">
+                <div class="container">
+                    <div class="blog-items">
+                        <?php 
+                        if ( have_posts()) : the_post();
+                            while ( have_posts() ) : 
+                                the_post();
+                        ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <div class="meta-info">
+                                    <p>Posted in by <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category();?></p>
+                                    <p>Tags: <?php the_tags(); ?></p>
+                                </div>
+                                <?php the_content(); ?>
+                            </article>
+                        <?php 
+                            endwhile;
+                        endif;?>
+                    </div>
+                </div>
+            </section>
+        </main>
     </div>
-    
-<?php get_footer();?>
+</div>
+
+<?php get_footer(); ?>
