c4be75517c1e2e984dfb72fa6cd906a62f3192bf
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-03-18 19:10:47 +0100

    Update dropdown.js

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,1 +1,14 @@
+let dropdown = document.querySelector('.meun'), //ul
+submenu = document.querySelector('.sub-menu'), //ul li a
+buttonclick = document.querySelector('.check-button'), //button
+hamburger = document.querySelector('.menu-icon');
+
+buttonclick.addEventListener('click', () =>{
+    dropdown.classList.toggle('show-dropdown');
+    if(submenu){
+        submenu.classList.toggle('show-dropdown');
+    }
+    hamburger.classList.toggle('animate-button');
+})
+
 console.log("Testing");
