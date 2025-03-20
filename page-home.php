92ecd6d321004ea167408ee28cd2e08ece680be5
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-03-20 19:11:32 +0100

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -1,1 +1,43 @@
-<p> test </p>
+<?php get_header(); ?>
+<img src="<?php header_image(); ?>" alt="" height="<?php echo get_custom_header()->height?>"
+width="<?php echo get_custom_header()->width?>">
+<!-- Hero Section -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="side-main">
+            <section class="hero">
+                Hero
+            </section>
+
+            <section class="services">
+                Services
+            </section>
+
+            <section class="home-blog">
+                <div class="container">
+                    <div class="blog-items">
+                        <?php 
+                        if ( have_posts()) : the_post();
+                            while ( have_posts() ) : 
+                                the_post();
+                        ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <div class="meta-info">
+                                    <p>Posted in by <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category();?></p>
+                                    <p>Tags: <?php the_tags(); ?></p>
+                                </div>
+                                <?php the_content(); ?>
+                            </article>
+                        <?php 
+                            endwhile;
+                        endif;?>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
