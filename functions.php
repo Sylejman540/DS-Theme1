4697d249bba13a73af23f097f4ed8320846aa7ce
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-04-01 19:10:19 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,36 +1,36 @@
-<?php 
+<?php
 
-function ds_theme_load_scripts(){
-        wp_enqueue_style('dstheme-style',get_stylesheet_uri(), array(), filemtime(get_template_directory().'/style.css'), 'all');
-        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' , array(), null);
-        wp_enqueue_script('dropdown', get_template_directory_uri(). 'js/dropdown.js', array(), '1.0', true);
+function wpdevs_load_scripts(){
+    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
+    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
 }
+add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
 
-add_action('wp_enqueue_scripts', 'ds_theme_load_scripts');
+
 
 function dstheme_config(){
-        register_nav_menus(
-                array(
-                        'wp_devs_main_menu' => 'Main Menu',
-                        'wp_devs_footer_menu' => 'Footer Menu'
-                )
-        );
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        )
+    );
 
-        $args = array(
-                'height' => 225,
-                'width' => 1920
-        );
+    $args = array(
+        'height' => 225,
+        'width' => 1920
+    );
 
-        add_theme_support('custom-header', $args);
-        add_theme_support('post-thumbnails');
-        add_theme_support('custom-logo', array(
-                'width' => 200,
-                'height' => 110,
-                'flex-height' => true,
-                'flex-width' => true
-        ));
+    add_theme_support('custom-header', $args);
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo', array(
+        'width' => 200,
+        'height' => 110,
+        'flex-height' => true,
+        'flex-width' => true
+    ));
 }
-
 add_action('after_setup_theme', 'dstheme_config', 0);
 
 add_action('widgets_init', 'dstheme_sidebars');
@@ -49,8 +49,8 @@ function dstheme_sidebars(){
     register_sidebar(
         array(
             'name' => 'Service 1',
-            'id' => 'services',
-            'description' => 'First Service Area.',
+            'id' => 'sevices-1',
+            'description' => 'First service Area',
             'before_widget' => '<div class = "widget-wrapper">',
             'after_widget' => '</div>',
             'before_title' => '<h4 class = "widget-title">',
@@ -61,7 +61,7 @@ function dstheme_sidebars(){
         array(
             'name' => 'Service 2',
             'id' => 'services-2',
-            'description' => 'Second Service Area.',
+            'description' => 'Second Service Area',
             'before_widget' => '<div class = "widget-wrapper">',
             'after_widget' => '</div>',
             'before_title' => '<h4 class = "widget-title">',
@@ -79,6 +79,5 @@ function dstheme_sidebars(){
             'after_title' => '</h4>'
         )
     );
-} 
+}
 
-?>
