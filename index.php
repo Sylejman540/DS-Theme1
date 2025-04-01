d3a88e6168d843476d3c5a7bdbaad241e451182b
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-04-01 19:12:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,38 @@
 <?php get_header(); ?>
-
-<!-- Hero Section -->
-<div id="content" class="site-content">
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <section class="hero">
-                Hero
-            </section>
-
-            <section class="services">
-                Services
-            </section>
-
-            <section class="home-blog">
-                <div class="container">
-                    <div class="blog-items">
-                        <?php 
-                        if ( have_posts()) : the_post();
-                            while ( have_posts() ) : 
-                                the_post();
-                        ?>
-                            <article>
-                                <h2><?php the_title(); ?></h2>
-                                <?php the_post_thumbnail(array(275,275)); ?>
-                                <div class="meta-info">
-                                    <p>Posted in by <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                    <p>Categories: <?php the_category();?></p>
-                                    <p>Tags: <?php the_tags(); ?></p>
-                                </div>
-                                <?php the_content(); ?>
-                            </article>
-                        <?php 
-                            endwhile;
-                        endif;?>
-                    </div>
-                    <?php get_sidebar()?>
-                </div>
-            </section>
-        </main>
-    </div>
-</div>
-
+<?php get_header(); ?>
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height ?>" 
+width="<?php echo get_custom_header()->width ?>" alt="">
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    
+                    <section class="home-blog">
+                        <div class="container">
+                            <div class="blog-items">
+                                <?php
+                                if( have_posts() ):
+                                    while( have_posts() ): the_post();
+                                        ?>
+                                            <article>
+                                                <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+                                                <a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(275,275)); ?></a> 
+                                                <div class="meta-info">
+                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link();?></p>
+                                                    <p>Catgeries: <?php the_category(); ?> </p>
+                                                    <p> <?php the_tags(); ?></p>
+                                                </div>
+                                                <?php the_excerpt(); ?>
+                                            </article>
+                                        <?php
+                                    endwhile;
+                                else: ?>
+                                    <p>Nothing to be displayed!</p>          
+                                <?php endif; ?>
+                            </div>
+                            <?php get_sidebar(); ?>
+                        </div>
+                    </section>
+                </main>
+            </div>
+        </div>
 <?php get_footer(); ?>
