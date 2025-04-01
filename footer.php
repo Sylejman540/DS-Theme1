c59c0f56a1fd24bc46e335b8e6b311c481e69300
SylejmanDurguti <durgutisylejman00@gmail.com>
2025-04-01 19:09:33 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
-    <!-- Footer -->
-    <footer class="site-footer">
-        <div class="container">
-            <div class="copyright">
-                <p>Copyright Sylejman - All rights reserved</p>
+<footer class="site-footer">
+            <div class="container">
+                <div class="copyright">
+                    <p>Copyright DS Team - All rights Reserved</p>
+                </div>
+                <nav class="footer-menu">
+                    <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)); ?>
+                </nav>
             </div>
-            <nav class="footer-menu">
-                <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1));?>
-            </nav>
-        </div>
-    </footer>
+        </footer>
+    </div>
     <?php wp_footer(); ?>
 </body>
 </html>
