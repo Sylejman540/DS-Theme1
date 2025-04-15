bff9bb31147eb532909662434be7803f07beff45
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-04-15 19:18:33 +0200

    Update search.php

--- search.php
+++ search.php
@@ -13,6 +13,7 @@
                 <article>
                     <header>
                         <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+                        <?php if( 'post' ==  get_post_type() );?>
                         <div class="meta-info">
                             <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link();?></p>
                             <p>Catgeries: <?php the_category( ' ' ); ?> </p>
