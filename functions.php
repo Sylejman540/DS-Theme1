a49ca757e99d4dfbcba96e45d4e8cde246235a70
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-04-29 19:14:34 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,18 @@
 <?php
+// Include customizer file
+require get_template_directory() . '/inc/customizer.php';
 
-function wpdevs_load_scripts(){
+// Enqueue scripts and styles
+function wpdevs_load_scripts() {
     wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
     wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
 
-
-
-function dstheme_config(){
+// Theme setup function
+function dstheme_config() {
+    // Register navigation menus
     register_nav_menus(
         array(
             'wp_devs_main_menu' => 'Main Menu',
@@ -17,11 +20,11 @@ function dstheme_config(){
         )
     );
 
+    // Add theme support
     $args = array(
         'height' => 225,
         'width' => 1920
     );
-
     add_theme_support('custom-header', $args);
     add_theme_support('post-thumbnails');
     add_theme_support('custom-logo', array(
@@ -33,8 +36,9 @@ function dstheme_config(){
 }
 add_action('after_setup_theme', 'dstheme_config', 0);
 
-add_action('widgets_init', 'dstheme_sidebars');
-function dstheme_sidebars(){
+// Register sidebars for widgets
+function dstheme_sidebars() {
+    // Register the blog sidebar
     register_sidebar(
         array(
             'name' => 'Blog Sidebar',
@@ -46,10 +50,12 @@ function dstheme_sidebars(){
             'after_title' => '</h4>'
         )
     );
+
+    // Register service sidebars
     register_sidebar(
         array(
             'name' => 'Service 1',
-            'id' => 'sevices-1',
+            'id' => 'services-1',
             'description' => 'First service Area',
             'before_widget' => '<div class = "widget-wrapper">',
             'after_widget' => '</div>',
@@ -80,4 +86,6 @@ function dstheme_sidebars(){
         )
     );
 }
+add_action('widgets_init', 'dstheme_sidebars');
 
+?>
