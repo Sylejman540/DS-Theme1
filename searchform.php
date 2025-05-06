db379a9aeb219cfa2c969a53085527473c749249
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-06 19:07:04 +0200

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo  esc_url(home_url( '/')) ?>">
    
     <div>
-       <label class="screen-reader-text" for="s">Search for:</label>
+       <label class="screen-reader-text" for="s"><?php esc_html_e('Search for:', 'wp-devs');?></label>
        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search..." />
-       <input type="submit" id="searchsubmit" value="Search" /> 
+       <input type="submit" id="searchsubmit" value="<?php esc_html_e('Search', 'wp-devs');?>"/> 
     </div>
 
 </form>
