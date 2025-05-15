c6be82dfd0fbf66a7d402c9727bd6e8bc06d3346
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-15 19:19:47 +0200

    Create footer.php

---
+++ Toothwise/footer.php
@@ -0,0 +1,14 @@
+<footer class="site-footer">
+  <div class="container footer-inner">
+    <div class="footer-left">
+      <p>&copy; <?php echo date('Y'); ?> Toothwise. All rights reserved.</p>
+    </div>
+    <div class="footer-right">
+      <p>123 Dental Ave, Smile City</p>
+      <p><a href="mailto:info@toothwise.com">info@toothwise.com</a></p>
+    </div>
+  </div>
+  <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
